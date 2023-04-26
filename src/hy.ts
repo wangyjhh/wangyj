d7ba33a86783db12db1a25486c4b370c4b1080b0
hikaru <827394320@qq.com>
2023-04-26 18:15:38 +0800

    完成切换镜像源命令，统一输出格式

--- src/hy.ts
+++ src/hy.ts
@@ -1,14 +1,25 @@
 #!/usr/bin/env node
 import { Command } from "commander"
 import chalk from "chalk"
+import inquirer from "inquirer"
 import { execSync } from "node:child_process"
-import pkg from "../package.json" assert { type: "json" }
-import registries from "../registries.json" assert { type: "json" }
+import pkg from "../package.json"
+import registries from "../registries.json"
 
 const program = new Command()
 
 const getOrigin = () => {
 	return execSync("npm get registry", { encoding: "utf-8" })
+}
+
+const setOrigin = (origin: string) => {
+	try {
+		execSync(`npm config set registry ${origin}`, { encoding: "utf-8" })
+		console.log(chalk.bgGreen(" 镜像源切换 "), chalk.green.bold(" 成功 "))
+	} catch (error) {
+		console.log(chalk.bgRed.white(" 镜像源切换 "), chalk.red.bold(" 错误 "))
+		console.log(chalk.bgRed.white(" 错误 "), chalk.red.bold(`${error}`))
+	}
 }
 
 program.name("jhy").description("npm镜像源操作命令")
@@ -19,8 +30,9 @@ program
 	.description("查看当前正在使用的镜像源")
 	.action(() => {
 		const res = getOrigin()
-		console.log(chalk.greenBright.bold(res))
+		console.log(chalk.bgGreen(" 当前镜像源 "), chalk.green(res))
 	})
+
 // 查看默认镜像源列表
 program
 	.command("ls")
@@ -32,6 +44,23 @@ program
 			registriesList.push({ name: k, registry: Reflect.get(registries as object, k).registry })
 		})
 		console.table(registriesList)
+	})
+
+// 切换镜像源
+program
+	.command("use")
+	.description("切换镜像源")
+	.action(async () => {
+		const { select } = await inquirer.prompt([
+			{
+				type: "list",
+				name: "select",
+				message: "请选择镜像源",
+				choices: Object.keys(registries),
+			},
+		])
+		const origin = Reflect.get(registries as object, select).registry
+		setOrigin(origin)
 	})
 
 program.version(pkg.version, "-V,--version", "输出版本号")
